25cd605ffc6889c8869e9f3ef13c20e46da1bd9d
mufid1012 <mufidghibran1012@gmail.com>
2024-03-26 14:29:57 +0700

    melengkapi prosedur

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 void inputData() //prosedur
 {
-    
+    cout  << "Masukan bilangan pertama : ";
+    cin >> bilangan 1;
+    cout <<  "Masukan bilangan kedua : ";
+    cin >> bilangan2;
 }
 
 int main() {
