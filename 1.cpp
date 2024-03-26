ab82275cf1f8398d73256ebcb836a737899255a6
mufid1012 <mufidghibran1012@gmail.com>
2024-03-26 14:44:00 +0700

    membuat fungsi pilihan

--- 1.cpp
+++ 1.cpp
@@ -32,10 +32,25 @@ float pembagian (int a,int b)
 }
 
 
-
-
-
 int main() {
+
+    int pilihan;
+
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukkan menu pilihan" << endl;
+        cin >> pilihan
+        
+
+    
 
 
 }
